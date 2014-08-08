78585a9812a058d953c393976f830c2580e27bd8
Andrey Kislyuk <kislyuk@gmail.com>
2014-08-08 09:05:48 -0700

    WIP

--- httpnext/__init__.py
+++ httpnext/__init__.py
@@ -1,28 +1,57 @@
 from __future__ import print_function, unicode_literals
 
-from asyncio import get_event_loop
+from asyncio import get_event_loop, Protocol
 
 # Should this inherit from both asyncio.protocol and HTTPConnection? (Probably not, but might be OK)
 
-class HTTPConnection(object):
+class HTTPConnection(Protocol):
     def __init__(self, host, port):
+        self.host = host
+        self.port = port
         self._loop = get_event_loop()
-        self._conn = self._loop.create_connection(FACTORY, host, port)
+        self._conn = None
 
     def connect(self):
-        pass
+        self._conn = self._loop.create_connection(lambda: self, self.host, self.port)
+        self._loop.run_until_complete(self._conn)
 
     def close(self):
         pass
+        #self._conn.close()
 
-    def request(method, url, body=None, headers={}):
-        pass
+    def request(self, method, url, body=None, headers={}):
+        self.transport.write("{} {}\n\n".format(method, url).encode())
+        self._loop.run_forever()        
 
     def getresponse(self):
         pass
 
     def __del__(self):
         self.close()
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print("Connection made:", transport)
+
+    def connection_lost(self, exc):
+        print('server closed the connection')
+        #asyncio.get_event_loop().stop()
+
+    def eof_received(self):
+        print("Received EOF")
+
+    def pause_writing(self):
+        print("Received pause writing cb")
+
+    def resume_writing(self):
+        print("Received resume writing cb")
+
+    def data_received(self, data):
+        print("Received data:", data)
+
+c = HTTPConnection("localhost", 9000)
+c.connect()
+c.request("GET", "/")
 
 class HTTPResponse(object):
     def __iter__(self):
